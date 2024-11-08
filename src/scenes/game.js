5207b9f3513473021d582dd30f9a960a68744ded
Neffer2 <n3f73r@gmail.com>
2024-11-07 23:19:10 -0500

    1. Avances drag and drop

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,6 +1,8 @@
 // Useful vars
-let width, height, mContext, player, referenciasTerpel = [], referenciasMobil = [], 
+let width, height, mContext, player,referencias1 = [],referencias2 = [], referencias3 = [], 
     gondola_terpel, gondola_mobil;
+
+const INIT_POSITION = 50, SPACE = 90;
 
 export class Game extends Phaser.Scene {
     constructor ()
@@ -12,9 +14,59 @@ export class Game extends Phaser.Scene {
         mContext = this;
         
         for(let i = 1; i < 6; i++){
-            let terpel = this.physics.add.image((gondola_terpel.x - 240), (height/4), `t-ref${i}`).setScale(.2);
-            referenciasTerpel.push(terpel);
+            let terpel = this.physics.add.image(0, 0, `t-ref${i}`).setScale(.2);
+            terpel.referencia = 'terpel';
+            (i < 3) ? referencias1.push(terpel) : referencias2.push(terpel);
         }
+
+        for(let i = 1; i < 6; i++){
+            let mobil = this.physics.add.image(0, 0, `m-ref${i}`).setScale(.2);
+            mobil.referencia = 'mobil';
+            (i < 3) ? referencias1.push(mobil) : referencias3.push(mobil);
+        }
+
+        referencias1.forEach((ref, i) => {
+            ref.x = (gondola_terpel.x - 250);
+            ref.y = 270;
+            ref.setInteractive({ draggable: true , dropZone: true });
+            ref.on('drag', function(pointer, gameObject, dragX, dragY){
+                ref.x = pointer.x;
+                ref.y = pointer.y;
+            }, mContext);
+
+            ref.on('drop', function(pointer, gameObject){
+                console.log(gondola_terpel.x);
+                console.log(gondola_terpel.x + gondola_terpel.width);
+                console.log(pointer.x);
+                if ((pointer.x > (gondola_terpel.x - gondola_terpel.width) && pointer.x < (gondola_terpel.x + gondola_terpel.width)) && 
+                    (pointer.y > (gondola_terpel.y - gondola_terpel.height) && pointer.y < (gondola_terpel.y + gondola_terpel.height))) {
+                    console.log('Bien');
+                }else {
+                    ref.x = (gondola_terpel.x - 250);
+                    ref.y = 270; 
+                }
+            });
+        });
+
+        referencias2.forEach((ref, i) => {
+            ref.x = (gondola_terpel.x - 250);
+            ref.y = 470;
+            ref.setInteractive({ draggable: true });
+            ref.on('drag', function(pointer, gameObject, dragX, dragY){
+                ref.x = pointer.x;
+                ref.y = pointer.y;
+            }, mContext);
+        });
+
+        referencias3.forEach((ref, i) => {
+            ref.x = (gondola_terpel.x - 250);
+            ref.y = 670;
+            ref.setInteractive({ draggable: true });
+            ref.on('drag', function(pointer, gameObject, dragX, dragY){
+                ref.x = pointer.x;
+                ref.y = pointer.y;
+            }, mContext);
+        });
 
     }
 
@@ -27,6 +79,7 @@ export class Game extends Phaser.Scene {
         height = this.sys.game.config.height;
 
         gondola_terpel = this.physics.add.image((width/2), (height/4), 'huevo1').setScale(1.5);
+        console.log(gondola_terpel);
         gondola_mobil = this.physics.add.image((width/2), (height/2), 'huevo2').setScale(1.5);
     }
 
